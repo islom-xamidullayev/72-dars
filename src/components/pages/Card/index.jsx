14ccb3631bef583f2eb39f081bf3c9d2d08a438a
unknown <isxm55203@gmail.com>
2024-12-24 20:09:46 +0500

    hammasi tugad

--- src/components/pages/Card/index.jsx
+++ src/components/pages/Card/index.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { updateQuantity } from '../store';
+import { NavLink } from 'react-router-dom';
+
+import { updateQuantity, removeFromCart } from '../../../store/store'; // removeFromCart actionini qo'shing
+import '../../../index.css';
+import Payment from '../payment';
 
 function Cart() {
   const cart = useSelector((state) => state.cart);
   const dispatch = useDispatch();
 
+  // Jami narxni hisoblash
+  const totalPrice = cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
+
   return (
-    <div className="p-4">
-      {cart.map((item) => (
-        <div key={item.id} className="flex justify-between items-center mb-4">
-          <h3 className="text-xl">{item.name}</h3>
-          <div className="flex items-center">
+    <div className="p-4 container">
+
+      {cart.map((item) => ( 
+        <div key={item.id} className="flex justify-between items-center w-[760px] mb-4 border-b pb-4">
+          <img src={item.image} className='w-[120px] h-[120px] mr-5' alt="" />
+          <h3 className="text-xl w-[260px]">{item.name}</h3>
+          <h4>${(item.price * item.quantity).toFixed(2)}</h4>
+          <div className="flex items-center w-[200px]">
             <button
               onClick={() => dispatch(updateQuantity({ productId: item.id, actionType: 'decrease' }))}
               className="px-2 py-1 border rounded"
@@ -26,8 +36,20 @@ function Cart() {
               +
             </button>
           </div>
+          <button
+            onClick={() => dispatch(removeFromCart(item.id))}
+            className="text-red-500 font-bold text-lg ml-4"
+          >
+            X
+          </button>
         </div>
       ))}
+      <div className="mt-4 text-right text-2xl font-semibold flex justify-between items-center">
+           <h1>Jami: ${totalPrice.toFixed(2)}</h1> 
+        <button className='py-3 px-14 bg-black text-[#fff] mr-5 rounded-lg'><NavLink to='/payment'>Sotib olish</NavLink> </button>
+    
+      </div>
+     
     </div>
   );
 }
